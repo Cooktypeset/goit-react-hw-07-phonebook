4176513e51b71efda29ddf8be05e0a4390adc07e
Cooktypeset <tovpyhalitva@gmail.com>
2023-09-18 23:20:27 +0300

    refReduxCont

--- src/Redux/ContactsSlice/ContactsSlice.js
+++ src/Redux/ContactsSlice/ContactsSlice.js
@@ -1,33 +1,25 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { InitialContactsState } from "./InitialContactsState";
-import { nanoid } from "nanoid";
+import { fetchContacts, deleteContacts, addContacts } from 'Redux/operations';
+import {
+    handleAddFulfilled,
+    handleDellFulfilled,
+    handleFetchFulfilled,
+    handlePending,
+    handleRejected,
+} from 'Redux/handlers';
+
 
 const ContactsSlice = createSlice({
     name: 'contact',
-    initialState: InitialContactsState,
-    reducers: {
-        addContact: {
-            reducer(state, action) {
-                state.contacts.push(action.payload);
-            },
-            prepare(name, number) {
-                return {
-                    payload: {
-                        id: nanoid(),
-                        name: name,
-                        number: number,
-                    },
-                };
-            },
-        },
-        deleteContact(state, action) {
-            const index = state.contacts.findIndex(
-                contact => contact.id === action.payload
-            );
-            state.contacts.splice(index, 1);
-        },
-    },
+    initialState:{items: [], loading:false, error: null},
+    extraReducers: builder => {
+        builder 
+         .addCase(fetchContacts.fulfilled, handleFetchFulfilled)
+      .addCase(addContacts.fulfilled, handleAddFulfilled)
+      .addCase(deleteContacts.fulfilled, handleDellFulfilled)
+      .addMatcher(action => action.type.endsWith('/pending'), handlePending)
+      .addMatcher(action => action.type.endsWith('/rejected'), handleRejected);
+    }
 });
 
 export const ContactsSliceReduser = ContactsSlice.reducer;
-export const { addContact, deleteContact } = ContactsSlice.actions;
